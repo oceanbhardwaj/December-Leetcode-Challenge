0c6c9255b19a4af5b22d61f67c00b04b0493b57f
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-12-10 23:15:39 +0530

    Add files via upload

---
+++ DeleteLeavesWithGivenValue.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        return dfs(root,target);
+       
+    }
+    public TreeNode dfs(TreeNode root,int target)
+    {
+        if(root==null)
+            return null;
+       root.left=dfs(root.left,target);
+        root.right=dfs(root.right,target);
+        if(root.left==null && root.right==null && root.val==target)
+            return null;
+   
+       
+        return root;    
+    }
+}
